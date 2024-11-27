8fc5decc3431ebeceee51312fb983fe097f3c94e
Ayush Pandey <pandeyayush956@gmail.com>
2024-11-27 21:24:49 +0530

    first commit

---
+++ 04_Dates.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+// console.log(myDate.toString);
+// console.log(myDate.toISOString());
+// console.log(myDate.toUTCString());
+// console.log(typeof myDate); // interview question
+
+let myCreatedDate = new Date(2023,0,23)
+console.log(myCreatedDate.toDateString());
