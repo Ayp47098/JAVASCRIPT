a84a9baf019e15e7ab510846dc4416c55871b8a5
Ayush Pandey <pandeyayush956@gmail.com>
2024-11-30 00:32:57 +0530

    json

--- 06_objects.js
+++ 06_objects.js
@@ -28,13 +28,96 @@ JsUser.email = "ayush@chatgpt.com"
 JsUser.email="ayush@microsoftservices.com"
 // console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
     
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
     
+// }
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
+
+// const tinderUser = new Object() // singleton object 
+const tinderUser = {} // non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.IsLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname: {
+        userfullname : {
+            firstname: "Ayush",
+            lastname: "Pandey"
+        }
+    }
 }
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+
+// console.log(regularUser.fullname?.userfullname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj4 = {5:"a",6:"b"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+
+const obj3 = {...obj1,...obj2}
+
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.IsLoggedIn.HasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "JAVASCRIPT",
+    price:"999",
+    courseInstructor:"ayush"
+}
+
+// course.courseInstructor
+
+const {courseInstructor:instructor} = course
+
+// console.log(courseInstructor);
+// console.log(instructor);
+
+// {
+//     "name":"ayush",
+//     "coursename":"JAVASCRIPT",
+//     "price":"444"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
